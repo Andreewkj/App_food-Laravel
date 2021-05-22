2f0f8f9cc22ba6a8ece3aebb9a22e6dc5542a983
andreew kennedy januario <andreew.k.januario@outlook.com>
2021-05-22 16:39:36 -0300

    implementando a função deletar comida

--- app/Http/Controllers/FoodController.php
+++ app/Http/Controllers/FoodController.php
@@ -107,6 +107,8 @@ class FoodController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $food = Food::find($id);
+        $food->delete();
+        return redirect()->route('food.index')->with('message','Food information updated');
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,10 +39,16 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
-                        @guest
+                        @if(Auth::check())
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                <a class="nav-link" href="{{ route('category.index') }}">{{ __('CATEGORY') }}</a>
                             </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('food.index') }}">{{ __('FOOD') }}</a>
+                            </li>
+                        @endif
+                        @guest
+                            
                             @if (Route::has('register'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
